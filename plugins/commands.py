4ffbbcb456d945442d511c5e632c57a63e78a290
allentertainmentsstudio <allentertainmentsstudio@gmail.com>
2026-04-01 14:14:33 +0530

    Add files via upload

---
+++ plugins/commands.py
@@ -0,0 +1,137 @@
+# Jishu Developer 
+# Don't Remove Credit 🥺
+# Telegram Channel @Madflix_Bots
+# Backup Channel @JishuBotz
+# Developer @JishuDeveloper
+
+
+
+
+import os
+import sys
+import asyncio 
+from database import db, mongodb_version
+from config import Config, temp
+from platform import python_version
+from translation import Translation
+from pyrogram import Client, filters, enums, __version__ as pyrogram_version
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaDocument
+
+main_buttons = [[
+        InlineKeyboardButton('📢 Updates', url='https://t.me/anujedits76'),
+        InlineKeyboardButton('💬 Support', url='https://t.me/anujedits76')
+        ],[
+        InlineKeyboardButton('🛠️ Help', callback_data='help'),
+        InlineKeyboardButton('🩷 About', callback_data='about')
+        ],[
+        InlineKeyboardButton('🧑‍💻 Developer 🧑‍💻', url='https://t.me/anujedits76')
+        ]]
+
+
+
+#===================Start Function===================#
+
+@Client.on_message(filters.private & filters.command(['start']))
+async def start(client, message):
+    user = message.from_user
+    if not await db.is_user_exist(user.id):
+        await db.add_user(user.id, user.first_name)
+    reply_markup = InlineKeyboardMarkup(main_buttons)
+    jishubotz = await message.reply_sticker("CAACAgUAAxkBAAECEEBlLA-nYcsWmsNWgE8-xqIkriCWAgACJwEAAsiUZBTiPWKAkUSmmh4E")
+    await asyncio.sleep(2)
+    await jishubotz.delete()
+    text = Translation.START_TXT.format(user.mention)
+    await message.reply_photo(
+        photo=Translation.PHOTO_URL,
+        caption=text,
+        reply_markup=reply_markup
+    )
+
+
+
+
+#==================Restart Function==================#
+
+@Client.on_message(filters.private & filters.command(['restart', "r"]) & filters.user(Config.OWNER_ID))
+async def restart(client, message):
+    msg = await message.reply_text(
+        text="<i>Trying To Restarting.....</i>",
+        quote=True
+    )
+    await asyncio.sleep(5)
+    await msg.edit("<i>Server Restarted Successfully ✅</i>")
+    os.execl(sys.executable, sys.executable, *sys.argv)
+    
+
+
+#==================Callback Functions==================#
+
+@Client.on_callback_query(filters.regex(r'^help'))
+async def helpcb(bot, query):
+    await query.message.edit_text(
+        text=Translation.HELP_TXT,
+        reply_markup=InlineKeyboardMarkup(
+            [[
+            InlineKeyboardButton('🛠️ How To Use Me 🛠️', callback_data='how_to_use')
+            ],[
+            InlineKeyboardButton('⚙️ Settings ⚙️', callback_data='settings#main'),
+            InlineKeyboardButton('📊 Stats 📊', callback_data='status')
+            ],[
+            InlineKeyboardButton('🔙 Back', callback_data='back')
+            ]]
+        ))
+
+
+
+@Client.on_callback_query(filters.regex(r'^how_to_use'))
+async def how_to_use(bot, query):
+    await query.message.edit_text(
+        text=Translation.HOW_USE_TXT,
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('🔙 Back', callback_data='help')]]),
+        disable_web_page_preview=True
+    )
+
+
+
+@Client.on_callback_query(filters.regex(r'^back'))
+async def back(bot, query):
+    reply_markup = InlineKeyboardMarkup(main_buttons)
+    await query.message.edit_text(
+       reply_markup=reply_markup,
+       text=Translation.START_TXT.format(
+                query.from_user.first_name))
+
+
+
+@Client.on_callback_query(filters.regex(r'^about'))
+async def about(bot, query):
+    await query.message.edit_text(
+        text=Translation.ABOUT_TXT.format(bot.me.mention),
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('🔙 Back', callback_data='back')]]),
+        disable_web_page_preview=True,
+        parse_mode=enums.ParseMode.HTML,
+    )
+
+
+
+@Client.on_callback_query(filters.regex(r'^status'))
+async def status(bot, query):
+    users_count, bots_count = await db.total_users_bots_count()
+    total_channels = await db.total_channels()
+    await query.message.edit_text(
+        text=Translation.STATUS_TXT.format(users_count, bots_count, temp.forwardings, total_channels, temp.BANNED_USERS ),
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('🔙 Back', callback_data='help')]]),
+        parse_mode=enums.ParseMode.HTML,
+        disable_web_page_preview=True,
+    )
+    
+
+
+
+
+
+# Jishu Developer 
+# Don't Remove Credit 🥺
+# Telegram Channel @Madflix_Bots
+# Backup Channel @JishuBotz
+# Developer @JishuDeveloper
